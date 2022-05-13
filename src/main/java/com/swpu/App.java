74cd079ec2713847712e592da90a4a808323a006
yumn <email@qq.com>
2022-05-13 16:36:10 +0800

    第一次提交

---
+++ src/main/java/com/swpu/App.java
@@ -0,0 +1,13 @@
+package com.swpu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/swpu/AppTest.java
@@ -0,0 +1,20 @@
+package com.swpu;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
